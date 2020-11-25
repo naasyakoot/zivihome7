acb7fc757f097fbc9de0b2cdf82cd667510d7587
naasyakoot <naasyakoot@outlook.com>
2020-11-25 15:13:21 +0600

    Stats

---
+++ js.js
@@ -0,0 +1,16 @@
+var data = function data() {
+
+    return {
+
+        name : 'SALAM',
+        color : 'white',
+        cf: 'container-fluid',
+        tw: 'display-1 text-white',
+        so: 'sometext',
+        ba: 'imgs/home-eight-banner-bg.jpg',
+        im: 'img-fluid'
+
+    };
+};
+
+Vue.createApp({data}).mount('#main');
